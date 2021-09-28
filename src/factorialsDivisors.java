5129276b0193a1f8268b319f967f393abaa98d5e
kaplanst <kaplanst@mail.ru>
2021-09-27 20:41:40 -0400

    Proba

--- src/factorialsDivisors.java
+++ src/factorialsDivisors.java
@@ -1,11 +1,9 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.*;
 
 public class factorialsDivisors {
     public static void main(String[] args) {
-        System.out.println(decomp(875));
+        System.out.println(decomp(10));
 
     }
     public static String decomp(int n) {
@@ -14,18 +12,21 @@ public class factorialsDivisors {
         for (int i = 2; i <= n; i++) {
             liCommon.addAll(aray(i));
         }
-        Collections.sort(liCommon);
-        LinkedHashSet<Integer> li2 = new LinkedHashSet<Integer>();
-
-         for (int i = 0; i < liCommon.size(); i++)
-            li2.add(liCommon.get(i));
-
-        System.out.println(li2);
-        List<Integer> li1 = new ArrayList<>(li2);
-        String resume = "";
+        System.out.println(liCommon);
+        List<Integer> li1 = new ArrayList<Integer>();
+        li1.add(liCommon.get(0));
+        for (int i = 1; i < liCommon.size(); i++) {
+            int t = 0;
+            for (int j = 0; j < li1.size(); j++) {
+                if (liCommon.get(i) == li1.get(j)) t++;
+            }
+            if (t == 0) li1.add(liCommon.get(i));
+        }
+        System.out.println(li1);
+        String resume;
         int temp = li1.get(0);
+        int[] baza = new int[li1.size()];
         for (int i = 0; i < li1.size(); i++) {
-            String helper = "";
             for (int j = i; j < li1.size(); j++) {
                 if (li1.get(i) > li1.get(j)) {
                     temp = li1.get(j);
@@ -34,21 +35,13 @@ public class factorialsDivisors {
                 }
 
             }
-            if (schotMnoj(liCommon, li1.get(i)) > 1) helper = "^" + schotMnoj(liCommon, li1.get(i));
-            resume += li1.get(i) + helper;
-            if (i != li1.size()-1) resume +=" * ";
-
+            baza[i] = schotMnoj(liCommon, li1.get(i));
         }
-
-        return resume;
+        return "Ura";
     }
-    public static int schotMnoj(List<Integer> istok, int stepen){
-        int c = 0;
-        int b;
-        for (int i = 0; i < istok.size(); i++) {
-            if (istok.get(i) == stepen) c++;
-        }
-        return c;
+    public static int schotMnoj(List istok, int stepen){
+
+        return 1;
     }
 
     public static List aray(int a) { // Разложим на множители входное число
@@ -63,4 +56,5 @@ public class factorialsDivisors {
         }
         return li;
     }
+
 }
---
+++ src/testClass.java
@@ -0,0 +1,103 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class testClass { // Приведение дроби в массиве к общему знаменателю
+
+    public static void main(String[] args) { // Результат окончательный
+        System.out.println(convertFrac(new long[][] {{1, 4}, {3, 6}, {4, 9}, {5, 7}}));
+    }
+    public static String convertFrac(long[][] lst) {
+        if (lst.length == 0) return "";
+        for (int k = 0; k < lst.length; k++) {
+            long tempDiv = 1;
+            for (long f = lst[k][0]; f >= 2; f--) {
+                if (lst[k][0] == 1) continue;
+                if (lst[k][0] % f == 0 && lst[k][1] % f == 0) {
+                    tempDiv = f;
+                    break;
+                }
+            }
+            lst[k][0] = lst[k][0] / tempDiv;
+            lst[k][1] = lst[k][1] / tempDiv;
+        }
+        int[] temp = new int[lst.length];
+        for (int i = 0; i < lst.length; i++) {
+            temp[i] = (int)lst[i][1];
+        }
+        int znam = prosto(temp);
+        String result = "";
+        for (int i = 0; i < lst.length; i++) {
+            result += "(" + (znam/lst[i][1]*lst[i][0]) + "," + znam + ")";
+
+        }
+        return result;
+    }
+    public static int prosto(int[] raz){ // Находим наименьшее общее кратное
+           for (int i = raz[raz.length - 1]; i < Integer.MAX_VALUE; i = i + raz[raz.length - 1]) {
+        int t = 0;
+        for (int j = 0; j<raz.length; j++) {
+            if (i % raz[j] == 0) t++;
+            else {
+                break;
+            }
+        }
+        if (t == raz.length) return i;
+    }
+            return 1;
+}
+}
+//    public static int prosto(int[] s){ // Находим наименьшее общее кратное
+//        int[][] dubl = new int[s.length][];
+//        for (int i = 0; i < s.length; i++) {
+//            dubl[i] = aray(s[i]);
+//        }
+//        List<Integer> li = new ArrayList<Integer>();
+//        for (int i = 0; i < dubl.length; i++) {
+//            for (int j = 0; j < dubl[i].length; j++) {
+//                li.add(dubl[i][j]);
+//            }
+//        }
+//        List<Integer> li1 = new ArrayList<Integer>();
+//        li1.add(li.get(0));
+//        for (int i = 1; i < li.size(); i++) {
+//            int t = 0;
+//            for (int j = 0; j < li1.size(); j++) {
+//                if (li.get(i) == li1.get(j)) t++;
+//            }
+//            if (t == 0) li1.add(li.get(i));
+//        }
+//        int nok = 1;
+//        for (int i = 0; i < li1.size(); i++) {
+//            nok = nok * (int) Math.pow(li1.get(i), schotMnojiteley(dubl, li1.get(i)));
+//        }
+//
+//        return nok;
+//    }
+//    public static int schotMnojiteley(int[][] arr, int s){ // Считаем количество множителей
+//        int sravDiv = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            int promDiv = 0;
+//            for (int j = 0; j < arr[i].length; j++) {
+//                if (arr[i][j] == s) promDiv++;
+//
+//            }
+//            if (sravDiv < promDiv) sravDiv = promDiv;
+////            System.out.println(sravDiv);
+//        }
+//        return sravDiv;
+//    }
+//
+//    public static int[] aray(int a) { // Разложим на множители входное число
+//        List<Integer> li = new ArrayList<Integer>();
+//        if (a ==1) return new int[]{1};
+//        int divider = 2;
+//        while (a / divider >= 1) {
+//            if (a % divider == 0) {
+//                li.add(divider);
+//                a = a / divider;
+//            }
+//            else divider++;
+//        }
+//        return li.stream().mapToInt(i->i).toArray();
+//    }
+//}
