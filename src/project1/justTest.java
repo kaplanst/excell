61db9248b63036a57fcc2c4f711c598f05acbaa1
kaplanst <kaplanst@mail.ru>
2021-09-28 22:09:51 -0400

    123

---
+++ src/project1/justTest.java
@@ -0,0 +1,9 @@
+package project1;
+
+public class justTest {
+    public static void main(String[] args) {
+        String s = "ddfdfdm,mkkjkkdmdkkdn,dm.dfldfld,fnkdmkdmld";
+        String p = s.replaceAll("d", "");
+        System.out.println(p);
+    }
+}
