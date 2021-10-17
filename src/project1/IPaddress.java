2dd7abc5fd3bd6d9fc1254c8d0a1bb8844ea5183
kaplanst <kaplanst@mail.ru>
2021-10-17 13:20:36 -0400

    New IP

--- src/project1/IPaddress.java
+++ src/project1/IPaddress.java
@@ -2,4 +2,41 @@ package project1;
 
 public class IPaddress {
 
+    public static void main(String[] args) {
+        System.out.println(longToIP(2149583361L));
+    }
+
+    public static String longToIP(long ip) {
+       String doubRes = "";
+        for (int i = 1; i <= 32; i++) {
+           doubRes += ip%2;
+           ip = ip / 2;
+           if (i % 8 == 0 && i != 32) doubRes += " ";
+       }
+        String reversRes = new StringBuilder(doubRes).reverse().toString();
+        String[] octets = reversRes.split(" ");
+        doubRes = "";
+        for (int i = 0; i < 4; i++) {
+        doubRes += convert(octets[i]);
+        if (i != 3) doubRes += ".";
+        }
+
+
+        return doubRes;
+    }
+    public static int convert(String zeros){
+        int octet = Integer.valueOf(zeros);
+        int digits = 0;
+        if (octet == 0) return 0;
+        for (int i = 0; i < 8; i++) {
+            digits += octet%10 * Math.pow(2, i);
+            octet = octet / 10;
+        }
+
+
+        return digits;
+    }
 }
+
+
+
--- src/project1/justTest.java
+++ src/project1/justTest.java
@@ -2,8 +2,7 @@ package project1;
 
 public class justTest {
     public static void main(String[] args) {
-        String s = "ddfdfdm,mkkjkkdmdkkdn,dm.dfldfld,fnkdmkdmld";
-        String p = s.replaceAll("d", "");
-        System.out.println(p);
+       double g = 5;
+        System.out.println(Math.pow(g, 4));
     }
 }
