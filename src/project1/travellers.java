ac51f59de25d9783ecfff0e7114315073962a462
Stanislav <kaplanst@mail.ru>
2021-12-28 17:10:47 -0500

    */Counting Duplicates Class added123

---
+++ src/project1/travellers.java
@@ -0,0 +1,20 @@
+package project1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class travellers {
+    public static void main(String[] args) {
+        List<Integer> ts = new ArrayList<>(Arrays.asList(50, 55, 56, 57, 58));
+        System.out.println(chooseBestSum(163, 3, ts));
+    }
+
+    public static Integer chooseBestSum(int t, int k, List<Integer> ls) {
+
+
+
+        return 4;
+    }
+}
+
