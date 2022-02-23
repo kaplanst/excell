ed9c036dcd827077f2763c123346b346ef25d3f0
Stanislav <kaplanst@mail.ru>
2022-02-23 10:56:49 -0500

    */Counting Duplicates Class added123

--- src/project1/justTest.java
+++ src/project1/justTest.java
@@ -1,9 +1,33 @@
 package project1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class justTest {
+
     public static void main(String[] args) {
-        String s = "ddfdfdm,mkkjkkdmdkkdn,dm.dfldfld,fnkdmkdmld";
-        String p = s.replaceAll("d", "");
-        System.out.println(p);
+        int input[] = { 1, 2, 3 };
+//        int input[] = {10,11,12,13};
+        possibleNumbers(input, 0);
+    }
+
+    public static void possibleNumbers(int[] x, int index) {
+        if (index == x.length) {
+            for (int i = 0; i < x.length; i++) {
+                System.out.print(x[i] + " ");
+            }
+            System.out.println();
+        }
+        for (int i = index; i < x.length; i++) {
+            int temp = x[index];
+            x[index] = x[i];
+            x[i] = temp;
+            possibleNumbers(x, index + 1);
+
+            temp = x[index];
+            x[index] = x[i];
+            x[i] = temp;
+        }
     }
 }
--- src/project1/travellers.java
+++ src/project1/travellers.java
@@ -11,10 +11,25 @@ public class travellers {
     }
 
     public static Integer chooseBestSum(int t, int k, List<Integer> ls) {
+        if (ls.size() < k) return null;
+        int just = Integer.MAX_VALUE;
+        Integer res = 0;
+        for (int i = 0; i < ls.size(); i++) {
+            for (int j = i+1; j < ls.size(); j++) {
+                for (int l = j+1; l < ls.size(); l++) {
+                    System.out.print((ls.get(i) + ls.get(j) + ls.get(l)) + ", ");
+
+                    if (just > t - (ls.get(i) + ls.get(j) + ls.get(l)) && t - (ls.get(i) + ls.get(j) + ls.get(l)) >= 0) {
+                        just = t - (ls.get(i) + ls.get(j) + ls.get(l));
+                        res = (ls.get(i) + ls.get(j) + ls.get(l));
+                    }
+                }
+             }
+        }
+        if (res == 0) return null;
 
 
-
-        return 4;
+        return res;
     }
 }
 
