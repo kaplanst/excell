56e2dde1c8f99489fd34ed4869e248ac98308da2
Stanislav <kaplanst@mail.ru>
2022-02-23 17:32:23 -0500

    */Counting Duplicates Class added123

---
+++ src/project1/Library.java
@@ -0,0 +1,29 @@
+package project1;
+public class Library {
+    public static void main(String[] args) {
+        String[] art = {"ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"};
+        String[] cd = new String[] {"A", "B"};
+        System.out.println(stockSummary(art, cd));
+    }
+    public static String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
+        String res = "";
+        for (String i: lstOf1stLetter) {
+            int count = sum(lstOfArt, i);
+            if (count > 0) res += " - (" + i + " : " + count + ")";
+        }
+        return res.substring(3); //       (A : 200) - (B : 1140)
+    }
+    public static int sum(String[] lstOfArt, String index) {
+        int total = 0;
+        for (String lst: lstOfArt) {
+            if (lst.contains(index)) {
+                total += Integer.parseInt(lst.substring(lst.indexOf(' ') + 1));
+            }
+        }
+
+        return total;
+    }
+
+}
+
+
--- src/project1/justTest.java
+++ src/project1/justTest.java
@@ -7,27 +7,11 @@ import java.util.List;
 public class justTest {
 
     public static void main(String[] args) {
-        int input[] = { 1, 2, 3 };
-//        int input[] = {10,11,12,13};
-        possibleNumbers(input, 0);
+
+        String s= "ASDFDSS 330";
+        int x = Integer.parseInt(s.substring(s.indexOf(' ') + 1));
+        System.out.println(x);
+
     }
 
-    public static void possibleNumbers(int[] x, int index) {
-        if (index == x.length) {
-            for (int i = 0; i < x.length; i++) {
-                System.out.print(x[i] + " ");
-            }
-            System.out.println();
-        }
-        for (int i = index; i < x.length; i++) {
-            int temp = x[index];
-            x[index] = x[i];
-            x[i] = temp;
-            possibleNumbers(x, index + 1);
-
-            temp = x[index];
-            x[index] = x[i];
-            x[i] = temp;
-        }
-    }
 }
