55e52cfb34f04931d4abc501c9eedb2a850e4382
kaplanst <kaplanst@mail.ru>
2022-04-17 12:21:20 -0400

    GitTest111222

--- src/project1/MergedString.java
+++ src/project1/MergedString.java
@@ -11,18 +11,22 @@ public class MergedString {
     public static boolean isMerge(String s, String part1, String part2) {
         if (s.length() != part1.length() + part2.length()) return false;
         char[] template = new char[s.length()];
+        int k = 0;
         for (int i = 0; i < part1.length(); i++) {
-            for (int j = 0; j < s.length(); j++) {
+            for (int j = k; j < s.length(); j++) {
                 if (part1.charAt(i) == s.charAt(j)) {
                     template[j] = s.charAt(j);
+                    k = j;
                     continue;
                 }
             }
          }
+        k = 0;
         for (int i = 0; i < part2.length(); i++) {
-            for (int j = 0; j < s.length(); j++) {
+            for (int j = k; j < s.length(); j++) {
                 if (part2.charAt(i) == s.charAt(j)) {
                     template[j] = s.charAt(j);
+                    k = j;
                     continue;
                 }
             }
