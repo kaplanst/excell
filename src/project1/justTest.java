faeef69a204be2ca2b399f2b145e8db9219b9fa5
kaplanst <kaplanst@mail.ru>
2022-05-01 12:58:18 -0400

    Encrypting

--- src/project1/justTest.java
+++ src/project1/justTest.java
@@ -6,24 +6,12 @@ import java.util.List;
 
 public class justTest {
     public static void main(String[] args) {
+        String s = "programming";
+        char[] c = s.toCharArray();
 
+        for (char el: c) {
 
+        }
 
     }
-
-
 }
-// 'udRsXusMT$? c%V;t_6r;NY ?2s94b8^7SWh>\*\g*cZ2udRsXusMT$? c%V;_eW6FLXU"(eDVbo<@GS/i5S#P*,95(FOI*.&C>'h?RB6lP/8;['
-// 'udRsXusMT$? c%V;_eW6FLXU"(eDVbo<@GS/i5S#P'
-// 'udRsXusMT$? c%V;t_6r;NY ?2s94b8^7SWh>\*\g*cZ2*,95(FOI*.&C>'h?RB6lP/8;['
-
-//            if (part1.charAt(0) == part2.charAt(0) && part1.length() > 1 && part2.length() > 1) {
-//                if (s.charAt(i + 1) == part1.charAt(1)) {
-//                    part1 = part1.substring(1);
-//                    System.out.print(s.charAt(i));
-//                } else {
-//                    part2 = part2.substring(1);
-//                    System.out.print(s.charAt(i));
-//                }
-//                continue;
-//            }
--- src/project1/repeat.java
+++ src/project1/repeat.java
@@ -3,7 +3,7 @@ package project1;
 public class repeat {
 
     public static void main(String[] args) {
-        String s = "programming";
+        String s = "12345677";
         int m = 0;
         for (int i = 0; i < s.length()-1; i++) {
             for (int j = i+1; j < s.length(); j++) {
