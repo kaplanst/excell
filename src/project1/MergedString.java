41ba624d21fe54788257d722a52112a7e77242f2
Stanislav <kaplanst@mail.ru>
2022-07-20 16:31:54 -0400

    */Counting Duplicates Class added123

--- src/project1/MergedString.java
+++ src/project1/MergedString.java
@@ -11,19 +11,10 @@ public class MergedString {
 // 'Can we merge it? Yes, we can!' is a merge of 'Cn rge i swa!' and 'a wemet?Ye, e cn'
     public static boolean isMerge(String s, String part1, String part2) {
         if (s.length() != part1.length() + part2.length()) return false;
-
+        if (s.length() == 0) return true;
+        if (part1.length() == 0) return s.equals(part2);
+        if (part2.length() == 0) return s.equals(part1);
         for (int i = 0; i < s.length(); i++) {
-
-            if (part1.charAt(0) == part2.charAt(0) && part1.length() > 1 && part2.length() > 1) {
-                if (s.charAt(i + 1) == part1.charAt(1)) {
-                    part1 = part1.substring(1);
-                    System.out.print(s.charAt(i));
-                } else {
-                    part2 = part2.substring(1);
-                    System.out.print(s.charAt(i));
-                }
-                continue;
-            }
 
             if (s.charAt(i) == part1.charAt(0)) {
                 System.out.print(part1.charAt(0));
--- src/project1/justTest.java
+++ src/project1/justTest.java
@@ -7,23 +7,16 @@ import java.util.List;
 public class justTest {
     public static void main(String[] args) {
 
+        System.out.println(multi(10));
+    }
 
+    public static int multi(int m){
+        if ( m >=1 ) {
+            return multi(m-1)*m;
+        }
 
+        return 1;
     }
 
 
 }
-// 'udRsXusMT$? c%V;t_6r;NY ?2s94b8^7SWh>\*\g*cZ2udRsXusMT$? c%V;_eW6FLXU"(eDVbo<@GS/i5S#P*,95(FOI*.&C>'h?RB6lP/8;['
-// 'udRsXusMT$? c%V;_eW6FLXU"(eDVbo<@GS/i5S#P'
-// 'udRsXusMT$? c%V;t_6r;NY ?2s94b8^7SWh>\*\g*cZ2*,95(FOI*.&C>'h?RB6lP/8;['
-
-//            if (part1.charAt(0) == part2.charAt(0) && part1.length() > 1 && part2.length() > 1) {
-//                if (s.charAt(i + 1) == part1.charAt(1)) {
-//                    part1 = part1.substring(1);
-//                    System.out.print(s.charAt(i));
-//                } else {
-//                    part2 = part2.substring(1);
-//                    System.out.print(s.charAt(i));
-//                }
-//                continue;
-//            }
