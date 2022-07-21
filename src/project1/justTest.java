8a96948a272e3878045e8e97ca30a2c21a0fbd51
Stanislav <kaplanst@mail.ru>
2022-07-21 13:33:03 -0400

    */Counting Duplicates Class added123

--- src/project1/justTest.java
+++ src/project1/justTest.java
@@ -7,15 +7,8 @@ import java.util.List;
 public class justTest {
     public static void main(String[] args) {
 
-        System.out.println(multi(10));
-    }
+        System.out.println('A' > 'B');
 
-    public static int multi(int m){
-        if ( m >=1 ) {
-            return multi(m-1)*m;
-        }
-
-        return 1;
     }
 
 
