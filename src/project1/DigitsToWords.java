5485e534d9347023620bdce10d024fe3e62d2d1c
kaplanst <kaplanst@mail.ru>
2022-12-14 13:41:47 -0500

    Digits to words

--- src/project1/DigitsToWords.java
+++ src/project1/DigitsToWords.java
@@ -1,12 +1,23 @@
 package project1;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class justTest {
+public class DigitsToWords {
     public static void main(String[] args) {
-        System.out.println(words(1));
+        System.out.println(millions(5045635004198L));
+    }
+
+    public static String millions(long allDig) {
+        if (allDig == 0) return "zero";
+        String res = "";
+        String[] millionsNames = {
+                "", " thousand", " million", " billion", " trillion"
+        };
+        for (String millionsName : millionsNames) {
+            if (allDig % 1000 != 0) res = words((int) (allDig % 1000)) + millionsName + " " + res;
+            if (allDig / 1000 == 0) break;
+            allDig = allDig / 1000;
+        }
+
+        return res.trim();
     }
     public static String words(int inp) {
         String[] tensNames = {
@@ -25,11 +36,9 @@ public class justTest {
                 inp = inp%100;
             }
         }
-
         if (inp < 20) return cont + numNames[inp];
         if (inp < 100 && inp%10 == 0) return cont + tensNames[inp/10];
         if (inp < 100) return cont + tensNames[inp/10] + "-" + numNames[inp%10];
         return "";
-
     }
 }
