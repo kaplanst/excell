4ca8d4b799e7d712e12bd301c1d09e61eea2cf70
kaplanst <kaplanst@mail.ru>
2023-01-13 17:38:08 -0500

    Telepon

--- src/project1/RomanNumerals.java
+++ src/project1/RomanNumerals.java
@@ -1,16 +1,32 @@
 package project1;
 
-import javax.imageio.stream.ImageInputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class justTest {
+public class RomanNumerals {
     public static void main(String[] args) {
+        System.out.println(solution(989));
+    }
+
+    public static String solution(int n) {
+        int tens = 10;
+        String res ="";
+        while (n > 0) {
+            System.out.println(n);
+            res = roma(n%tens, tens/10) + res;
+            n = n / tens;
+            tens = tens *10;
+        }
+
+        return res;
+    }
+
+    public static String roma(int dig, int tens){
         List<Integer> temp = new ArrayList<>();
-        int tens = 1;
-        int dig = 7;
-        switch (dig) {
+        int[] value = new int[] {1, 5, 10, 50, 100, 500, 1000};
+        String[] romSymb = new String[] {"I", "V", "X", "L", "C", "D", "M"};
+        String str = "";
+        switch(dig) {
             case 0:
                 temp.add(0);
                 break;
@@ -45,6 +61,12 @@ public class justTest {
                 temp.add(tens); break;
         }
         System.out.println(temp);
+        for (int i = 0; i < temp.size(); i++) {
+            for (int j = 0; j < value.length; j++) {
+                if (temp.get(i) == value[j]) str += romSymb[j];
+            }
+        }
+        return str;
     }
 
 }
