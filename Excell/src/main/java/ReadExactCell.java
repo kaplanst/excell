13246cc2511efb69ce1dfa0f55bd1fde8c5df497
kaplanst <kaplanst@mail.ru>
2023-06-06 19:24:57 -0400

    qwe

---
+++ Excell/src/main/java/ReadExactCell.java
@@ -0,0 +1,42 @@
+import com.aspose.cells.Workbook;
+import com.aspose.cells.Worksheet;
+import com.aspose.cells.WorksheetCollection;
+
+public class ReadExactCell {
+    public static void main(String[] args) throws Exception {
+        // Load Excel file
+        Workbook wb = new Workbook("C:\\employment.xlsx");
+
+// Get all worksheets
+        WorksheetCollection collection = wb.getWorksheets();
+
+// Loop through all the worksheets
+        for (int worksheetIndex = 0; worksheetIndex < collection.getCount(); worksheetIndex++) {
+
+            // Get worksheet using its index
+            Worksheet worksheet = collection.get(worksheetIndex);
+
+            // Print worksheet name
+            System.out.print("Worksheet: " + worksheet.getName());
+
+            // Get number of rows and columns
+            int rows = worksheet.getCells().getMaxDataRow();
+            int cols = worksheet.getCells().getMaxDataColumn();
+
+            // Loop through rows
+            for (int i = 0; i < rows; i++) {
+
+                // Loop through each column in selected row
+                for (int j = 0; j < cols; j++) {
+                    // Pring cell value
+                    System.out.print(worksheet.getCells().get(i, j).getValue() + " | ");
+                }
+                // Print line break
+                System.out.println(" ");
+            }
+        }
+    }
+}
+
+
+
--- src/project1/justTest.java
+++ src/project1/justTest.java
@@ -1,33 +1,36 @@
 package project1;
 
-import javax.imageio.stream.ImageInputStream;
-import java.io.BufferedReader;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-//test
+
 public class justTest {
-    public static void main(String[] args) throws IOException {
-        URL url = new URL("https://jsonmock.hackerrank.com/api/countries?name=italy");
-        URLConnection con = url.openConnection();
-        InputStream is = con.getInputStream();
-        String content = null;
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                content = line;
-            }
-        }
-        String capital = content.substring(content.lastIndexOf("capital\":\"") + 10);
-        capital = capital.substring(0, capital.indexOf('"'));
-        System.out.println(capital);    }
+
+    public static void main(String[] args) {
+
+ //       String path = "C:\\Book1.xlsx";
+
+//        try {
+//
+//            //Create an object of FileInputStream class to read excel file
+//            FileInputStream fis = new FileInputStream(path);
+//
+//            //Create object of XSSFWorkbook class
+//            XSSFWorkbook wb = new XSSFWorkbook(fis);
+//
+//            //Read excel sheet by sheet name
+//            XSSFSheet sheet1 = wb.getSheet("Sheet2");
+//
+//            //Get data from specified cell
+//            System.out.println(sheet1.getRow(1).getCell(1).getStringCellValue());
+//            System.out.println(sheet1.getRow(3).getCell(0).getStringCellValue());
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+    }
 
 }
-
-
